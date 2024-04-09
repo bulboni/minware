56e80e647feba60c23c3a3bc4564171cd753a588
bulboni <163756439+bulboni@users.noreply.github.com>
2024-04-09 09:11:44 -0400

    Update proxm.py

--- proxm.py
+++ proxm.py
@@ -34,7 +34,7 @@ def handle_client(client_socket, remote_host, remote_port):
 
 def main():
     local_host = "0.0.0.0"  # Bind to all available interfaces
-    local_port = 4000  # Change to desired local port
+    local_port = 4040  # Change to desired local port
     remote_host = "pool.hashvault.pro"  # Change to target server address
     remote_port = 7777  # Change to target server port
     
